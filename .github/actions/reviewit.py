0012365a364bf1aa7db447f854100fcd60e209a0
Sachin Uplaonkar <up.sachi@gmail.com>
2024-03-04 12:06:51 -0800

    Update reviewit.py

--- .github/actions/reviewit.py
+++ .github/actions/reviewit.py
@@ -53,8 +53,8 @@ def get_file_contents(repo_name, pr_number):
 def main(repo_name, pr_number):
     pr_contents = get_file_contents(repo_name, pr_number)
     click.echo(pr_contents)
-    # reviews = get_code_review(pr_contents)
-    # click.echo(reviews)
+    reviews = get_code_review(pr_contents)
+    click.echo(reviews)
 
 
 if __name__ == "__main__":
