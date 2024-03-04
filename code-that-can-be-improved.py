add96133cd0a9cc35def7e9cc077386210bf5998
sachs7 <up.sachi@gmail.com>
2024-03-04 13:00:34 -0800

    Create code-that-can-be-improved.py

---
+++ code-that-can-be-improved.py
@@ -0,0 +1,11 @@
+def func_x(num):
+    if num == 1:
+        return a()
+    elif num == 2:
+        return b()
+    elif num == 3:
+        return c()
+    elif num == 4:
+        return d()
+    elif num == 5:
+        return e()
